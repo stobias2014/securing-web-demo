631942c89722538a108a0f08ad358455ad011f4d
Saul Tobias <saul.tobias@fdmgroup.com>
2020-06-28 21:54:44 -0500

    added securing-web-demo

---
+++ src/main/java/com/tobias/saul/securingwebdemo/SecuringWebDemoApplication.java
@@ -0,0 +1,13 @@
+package com.tobias.saul.securingwebdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuringWebDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuringWebDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tobias/saul/securingwebdemo/SecuringWebDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tobias.saul.securingwebdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuringWebDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
